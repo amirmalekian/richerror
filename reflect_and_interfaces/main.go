ca8b64919bd574aa8fb598c2a298c4e6ae45fce5
amirmalekian <amirmalekian.dev@gmail.com>
2026-04-27 18:30:54 +0330

    intro to reflect concept

--- reflect_and_interfaces/main.go
+++ reflect_and_interfaces/main.go
@@ -2,122 +2,36 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"reflect_and_interfaces/log"
+	"reflect"
 	"reflect_and_interfaces/richerror"
-	"reflect_and_interfaces/simpleerror"
-	"strconv"
 	"time"
 )
 
-type User struct {
-	ID   uint
-	Name string
-}
-
-func (u User) String() string {
-	return fmt.Sprintf("User{id: %d, name: %s}", u.ID, u.Name)
-}
-
 func main() {
-	logger := log.Log{}
-
-	u := User{ID: 123, Name: "Amirhossein"}
-	fmt.Println(u)
-	// fmt.Stringer
-
-	_, OErr := os.OpenFile("./storage/data.txt", os.O_RDWR, 0777)
-	if OErr != nil {
-		logger.Append(OErr)
-		// fmt.Println(OErr.Error())
-		fmt.Println(OErr.Error())
-
-		fmt.Println(OErr)
+	rErr := richerror.RichError{
+		Message: "id is not valid",
+		MetaData: map[string]string{
+			"id": "0",
+		},
+		Operation: "main",
+		Time:      time.Now(),
 	}
 
-	user, gErr := getUserByID(0)
-	// gErr.Error()
-	if gErr != nil {
-		logger.Append(gErr)
-		fmt.Println(gErr.Error())
-		// // type assertion => برای اینکه به تایپ واقعی برسیم ، به اون کانکریت تایپه ، کانکریت ولیو که پاس داده شده به عنوان اینترفیس برسیم
-		// rErr, ok := gErr.(*richerror.RichError)
-		// if ok {
-		// 	logger.Append(rErr)
-		// } else {
-		// 	logger.Append(&richerror.RichError{
-		// 		Message:   gErr.Error(),
-		// 		MetaData:  nil,
-		// 		Operation: "unknown",
-		// 	})
-		// }
-		// fmt.Println(err.Error())
-		// logger.Errors = append(logger.Errors, gErr)
-		// logger.Append(gErr)
-	}
+	value := reflect.ValueOf(rErr)
+	fmt.Println("kind", value.Kind())
+	fmt.Println("type", reflect.TypeOf(rErr))
 
-	_, g2Err := getUserByIDTwo(0)
-	fmt.Println("operation", g2Err.Operation, g2Err.Message, g2Err.MetaData)
+	switch value.Kind() {
+	case reflect.Struct:
+		fmt.Println("number of field", value.NumField())
+		for i := 0; i < value.NumField(); i++ {
+			fieldValue := value.Field(i)
+			fmt.Printf("field index: %d, type: %s, field-name: %s, value: %s\n", i, fieldValue.Type(), value.Type().Field(i).Name, fieldValue.Interface())
+			//fieldType := value.Type().Field(i)
 
-	if g2Err != nil {
-		// fmt.Println(err.Error())
-		// logger.Errors = append(logger.Errors, gErr)
-		logger.Append(g2Err)
-		fmt.Println(g2Err.Error())
-
-	}
-
-	_, g3Err := getUserByIDThree(0)
-	if g3Err != nil {
-		logger.Append(g3Err)
-		fmt.Println(g3Err.Error())
-
-	}
-
-	logger.Save()
-
-	fmt.Println("user", user)
-}
-
-// abstraction type
-func getUserByID(id int) (User, error) {
-	if id == 0 {
-		return User{}, &richerror.RichError{
-			Message: "id is not valid",
-			MetaData: map[string]string{
-				"id": strconv.Itoa(id),
-			},
-			Operation: "getUserByID",
-			Time:      time.Now(),
 		}
+	default:
+		fmt.Println("kind", value.Kind())
 	}
 
-	return User{}, nil
-}
-
-// concrete type تایپ واقعی
-func getUserByIDTwo(id int) (User, *richerror.RichError) {
-	if id == 0 {
-		return User{}, &richerror.RichError{
-			Message: "id is not valid",
-			MetaData: map[string]string{
-				"id": strconv.Itoa(id),
-			},
-			Operation: "getUserByID",
-			Time:      time.Now(),
-		}
-	}
-
-	return User{}, nil
-}
-
-func getUserByIDThree(id int) (User, error) {
-	if id == 0 {
-		return User{}, &simpleerror.SimpleError{
-			Output:    "id is 0",
-			Operation: "getUserByIDThree",
-		}
-	}
-
-	return User{}, nil
 }
